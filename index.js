5a6df153746411b404d6fdb52e27ea5d297313f5
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-22 14:39:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-function addingEventListener() {
-    let input = document.getElementById('button');
-input.addEventListener('click', function() {
-  alert('I was clicked!');
-});
+// function addingEventListener() {
+//     let input = document.getElementById('button');
+// input.addEventListener('click', function() {
+//   alert('I was clicked!');
+// });
+// }
+
+function addingEventListener(){
+  let input = document.getElementById('button');
+  input.addEventListener('click', clickAlert)
+}
+function clickAlert(){
+  alert('I was clicked')
 }
